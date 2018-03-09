07bdd2435f923cb672fa0d5471f3580bbac69ef7
Devin DiNapoli <devindinapoli@gmail.com>
2018-03-09 16:32:21 -0500

    Updated js, all should work now

--- timesheet.js
+++ timesheet.js
@@ -1,50 +1,56 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyC2Us3l2Xt4FD7M8AorSeZooz_2BhWKepg",
-    authDomain: "timesheet-62ce4.firebaseapp.com",
-    databaseURL: "https://timesheet-62ce4.firebaseio.com",
-    projectId: "timesheet-62ce4",
-    storageBucket: "timesheet-62ce4.appspot.com",
-    messagingSenderId: "382375287681"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC2Us3l2Xt4FD7M8AorSeZooz_2BhWKepg",
+  authDomain: "timesheet-62ce4.firebaseapp.com",
+  databaseURL: "https://timesheet-62ce4.firebaseio.com",
+  projectId: "timesheet-62ce4",
+  storageBucket: "timesheet-62ce4.appspot.com",
+  messagingSenderId: "382375287681"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
-  var name = "";
-  var role = "";
-  var start = "";
-  var monthRate = "";
-  var monthsWorked = "";
-  var totalBilled = "";
+var database = firebase.database();
+var name = "";
+var role = "";
+var start = "";
+var monthRate = "";
 
-  $("#submit-button").on("click", function(event){
-    event.preventDefault();
+$("#submit-button").on("click", function(event){
+  event.preventDefault();
 
+  name = $("#name-input").val().trim();
+  role = $("#role-input").val().trim();
+  start = moment($("#start-input").val().trim(), "MM/DD/YY").format("X");
+  monthsWorked = "";
+  monthRate = $("#rate-input").val().trim();
+  totalBilled = "";
 
-    name = $("#name-input").val().trim();
-    role = $("#role-input").val().trim();
-    start = moment($("#start-input").val().trim(), "DD/MM/YY").format("X");
-    monthsWorked = ""
-    monthRate = $("#rate-input").val().trim();
-    totalBilled = ""
-
-    
-  
-    database.ref().push({
-        name: name,
-        role: role,
-        start: moment(start, "MM/DD/YY").format("X"),
-        rate: monthRate
-    });
-
-
+  database.ref().push({
+    name: name,
+    role: role,
+    start: start,
+    rate: monthRate
   });
 
-  database.ref().on("child_added", function (childSnapshot) {
-      //  monthsWorked = exFunction(a, b)
-    var newStart = moment(start).format("MMMM Do YYYY")
-    $("#table-body").append("<tr>" + "<td>" + childSnapshot.val().name + "</td><td>" + childSnapshot.val().role + "</td><td>" + childSnapshot.val().newStart +
-    "</td><td>" + childSnapshot.val().monthsWorked + "</td><td>" + childSnapshot.val().rate + "</td><td>" + childSnapshot.val().totalBilled + "</td><?tr>");
+  $("#name-input").val("");
+  $("#role-input").val("");
+  $("#start-input").val("");
+  $("#rate-input").val("");
+
+});
+
+database.ref().on("child_added", function (childSnapshot) {
+  //  monthsWorked = exFunction(a, b)
+  var start = childSnapshot.val().start  
+  var rate = childSnapshot.val().rate;
+  var name = childSnapshot.val().name;
+  var role = childSnapshot.val().role;
+  var newStart = moment.unix(start).format("MM/DD/YY");
+  var monthsWorked = moment().diff(moment.unix(start, "X"), "months");
+  var totalBilled = monthsWorked * rate;
+
+  $("#table-body").append("<tr>" + "<td>" + name + "</td><td>" + role + "</td><td>" + newStart +
+  "</td><td>" + monthsWorked + "</td><td>" + rate + "</td><td>" + totalBilled + "</td><tr>");
   }, function (errorObject) {
-    console.log("Errors handled: " +errorObject.code);
+  console.log("Errors handled: " +errorObject.code);
   });
